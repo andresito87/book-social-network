9374064a8efc7aa4e30d02432c50abba3b9579c6
Andres <andrespodadera87@gmail.com>
2024-06-23 21:47:52 +0200

    first commit

---
+++ book-network/src/main/java/com/andres/book/secutiry/UserDetailsService.java
@@ -0,0 +1,22 @@
+package com.andres.book.secutiry;
+
+import com.andres.book.user.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+    
+    private final UserRepository repository;
+    
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        return repository.findByEmail(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found"));
+    }
+}
