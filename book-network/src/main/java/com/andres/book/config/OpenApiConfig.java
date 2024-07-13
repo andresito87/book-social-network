71d5458a76b55e56d9ca87756c039455446aec02
Andres <andrespodadera87@gmail.com>
2024-07-13 18:42:28 +0200

    creating openapi specification,config and generating docs in frontend

--- book-network/src/main/java/com/andres/book/config/OpenApiConfig.java
+++ book-network/src/main/java/com/andres/book/config/OpenApiConfig.java
@@ -10,6 +10,8 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 
+// Link to Documentation: http://localhost:8088/api/v1/swagger-ui/index.html
+
 @OpenAPIDefinition(
         info = @Info(
                 contact = @Contact(
