f55eb47af1c7ff34b75a0897e7ce1e7b10849c91
Andres <andrespodadera87@gmail.com>
2024-07-13 16:18:39 +0200

    implemented to give feedbacks functionality

---
+++ book-network/src/main/java/com/andres/book/feedback/FeedbackMapper.java
@@ -0,0 +1,32 @@
+package com.andres.book.feedback;
+
+import com.andres.book.book.Book;
+import com.andres.book.user.User;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+public class FeedbackMapper {
+    public Feedback toFeedback(FeedbackRequest request, User user, Book book) {
+        return Feedback.builder()
+                .note(request.note())
+                .comment(request.comment())
+                .book(Book.builder()
+                        .id(request.bookId())
+                        .archived(false)
+                        .shareable(true)
+                        .build())
+                .build();
+    }
+
+    public FeedbackResponse toFeedbackResponse(Feedback feedback, Integer id) {
+        
+        return FeedbackResponse.builder()
+                .note(feedback.getNote())
+                .comment(feedback.getComment())
+                .ownFeedback(Objects.equals(feedback.getCreatedBy(), id))
+                .build();
+        
+    }
+}
